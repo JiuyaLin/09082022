ed75a17c14c884570ecef6d74894a230a6c61698
Eva <jl11935@nyu.edu>
2022-09-14 19:41:01 -0400

    Lab Sep 14

---
+++ Assignment2_Flatgame/Assets/Script/scr_playerMove.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_playerMove : MonoBehaviour
+{
+    public float speed;
+    public float decelerate;
+    private float hSpeed = 0;
+    private float vSpeed = 0;
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 curPos = transform.position;
+        hSpeed = (float)(decelerate * hSpeed);
+        Debug.Log("hSpeed: " + hSpeed);
+        vSpeed = (float)(decelerate * vSpeed);
+        Debug.Log("vSpeed: " + vSpeed);
+        //trim down the number
+        hSpeed = (float)(Mathf.Ceil(hSpeed * 10) / 10.0);
+        //vSpeed = (float)(Mathf.Ceil(vSpeed * 10) / 10);
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            hSpeed = -speed;
+        }else if (Input.GetKey(KeyCode.D))
+        {
+            hSpeed = speed;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            vSpeed = speed;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            vSpeed = -speed;
+        }
+        
+
+        curPos.x += hSpeed;
+        curPos.y += vSpeed;
+
+        transform.position = curPos;
+
+    }
+}
