955a203d26d474c09518587c6af12747b420425c
JiuyaLin <jl11935@nyu.edu>
2022-09-19 11:10:24 -0400

    Attempt to fix but fail. Re-export HTML.

--- Assignment2_Flatgame/Assets/Script/scr_camerFollow.cs
+++ Assignment2_Flatgame/Assets/Script/scr_camerFollow.cs
@@ -24,7 +24,20 @@ public class scr_camerFollow : MonoBehaviour
         camPos = playerCam.transform.position;
         mcPos = player.transform.position;
 
-        camPos.x = relativePos.x + mcPos.x;
+        //I will just hard-coded the position for now since that is actually easier
+        if(camPos.x > -0.38 && camPos.x < 80.81)
+        {
+            camPos.x = relativePos.x + mcPos.x;
+            Debug.Log("normal move");
+        }else if(camPos.x < -0.38)
+        {
+            camPos.x = (float)-0.379;
+            Debug.Log("left Bound");
+        }else if(camPos.x > 80.81)
+        {
+            camPos.x = (float)80.8;
+        }
+        
 
         playerCam.transform.position = camPos;
         Debug.Log("moved");
