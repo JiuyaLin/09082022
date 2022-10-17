161884c033293b8cb0b3e75f06f1ab06dd88bf89
Eva <jl11935@nyu.edu>
2022-10-17 18:33:52 -0400

    playtested platformer

---
+++ Assignment4-platformer/Assets/notes.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+
+
+//1. start fast (double jump fast is fun), top slow, and then goes down fast again
+//dynamic music
+//particle effects
+//tiral render
+//animation: squash and stretch.
+
